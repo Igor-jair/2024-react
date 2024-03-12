eeedd24444cecff0dec777c81d3add5904c73e5c
Igor-jair <105497607+Igor-jair@users.noreply.github.com>
2024-03-12 17:38:15 -0300

    atualização dos botoes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import "./App.css";
+
 const MeuBotao = (props: any) => {
     return <button>{props.titulo}</button>;
 }
 
 const App = () => {
     return (
-      <div>
+      <div className="aplicacao">
         <h1>Bem vindo ao mundo React</h1>
         <MeuBotao titulo="primeiro botao"/>
         <MeuBotao titulo="segundo botao"/>
